42c8362eff8880a2d6fff2d07d12049a0d2a3ce3
berkan bala <berkan.balaa@gmail.com>
2024-01-17 15:53:57 +0300

    sccs files fixed

--- src/common/components/ui/loading/loading.tsx
+++ src/common/components/ui/loading/loading.tsx
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 import classnames from "classnames";
 import styles from "./loading.module.scss";
 
 export const Loading = ({ className }: Props) => {
   return (
     <div className={classnames(styles.container, className)}>
-=======
-import styles from "./loading.module.scss";
-import classNames from "classnames";
-export const Loading = ({ className }: Props) => {
-  return (
-    <div className={classNames(styles.container, className)}>
->>>>>>> ec06fc633963c974a05f46647448562ed22c95e7
       <div className={styles.loadingIcon} />
     </div>
   );
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,7 @@
 import { Loading } from "../../common/components/ui/loading/loading";
 import { Character } from "../../custom/components/character/character";
 import styles from "./home.module.scss";
-<<<<<<< HEAD
-import { Loading } from "common/components/ui/loading/loading";
-=======
 import { useGetMarvel } from "../../common/hooks/useGetMarvel";
->>>>>>> ec06fc633963c974a05f46647448562ed22c95e7
 
 export const Home = () => {
   const { characters, charactersLoading, charactersError } = useGetMarvel();
