56b9d244791707d2683e0162fdc9ce886d4ff440
berkan bala <berkan.balaa@gmail.com>
2024-01-18 15:19:23 +0300

    theme changed

--- src/pages/notfound/notFound.tsx
+++ src/pages/notfound/notFound.tsx
@@ -4,10 +4,10 @@ import IronMan from "../../common/media/images/ironMan.png";
 export const NotFound = () => {
   return (
     <div className={styles.container}>
+      <h1>404 Page Not Found</h1>
       <div className={styles.content}>
-        <h1>404 Page Not Found</h1>
         <div className={styles.image}>
-          <img alt="Marvel Not Found" src={IronMan} />
+          <img alt="Marvel Not Found" src={IronMan} width={400} height={550} />
         </div>
       </div>
     </div>
